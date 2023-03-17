2ab99e9c031b2c0c1fae8e35fc927b83576a6de9
jswamy-aires <67158625+jswamy-aires@users.noreply.github.com>
2023-03-18 03:19:51 +0530

    Update Dashboard.jsx

--- src/pages/views/dashboard/Dashboard.jsx
+++ src/pages/views/dashboard/Dashboard.jsx
@@ -1,16 +1,14 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import Arc from "../grid/Arc";
- import DashboardGrid from "../grid/DashboardGrid";
+import DashboardGrid from "../grid/DashboardGrid";
 //import GridWithDrawer from "../grid/GridWithDrawer";
 
 
 const Dashboard = () => {
     return <Typography>
-             <Typography variant="h1">Dashboard</Typography>
-             <DashboardGrid />
-
-             </Typography>
+        <Typography variant="h1">Dashboard</Typography>
+        <DashboardGrid />
+    </Typography>
 };
 
 export default Dashboard;
